30e2526231b46b8aef44d19ca4566663a3308887
Francisco Carmona <flcarmonac@yahoo.com>
2024-03-08 13:19:53 -0300

    Calculadora

--- Calculadora/assets/js/script.js
+++ Calculadora/assets/js/script.js
@@ -1,28 +1,50 @@
 const resultado = document.querySelector("#resultado");
 
+//se define los operadores permitidos y las variables
+const operadores = ["*", "/", "+", "-"];
+const calcular = [];
 let operador = "";
-let aux = 0;
-let valor = "";
+let nuevoNum = ""
 
 function handleClickNum(e) {
-  const nuevoNum = document.querySelector(`#${e.id}`).innerHTML;
-  valor = valor + nuevoNum;
-  resultado.innerHTML = valor;
+  nuevoNum = nuevoNum + e.innerHTML;
+  resultado.innerHTML = nuevoNum;
 };
 
 function handleClickOperador(e) {
-  operador = document.querySelector(`#${e.id}`).innerHTML;
-  aux = valor;
-  valor = "";
+  // se obtiene el operador (+, -, *, /)
+  operador = e.innerHTML;
+  // se guarda en el arreglo el numero y el operador
+  calcular.push(nuevoNum);
+  calcular.push(operador);
+  //
+  nuevoNum = "";
   resultado.innerHTML = operador;
 }
 
-function handleClickIgual(e) {
-  resultado.innerHTML = eval(`${aux} ${operador} ${valor}`);
-  operador = "";
-  valor = "";
+function handleClickIgual() {
+  // numNuevo tiene valor se agrega al arreglo
+  if (nuevoNum !== "") calcular.push(nuevoNum);
+  // si el utimo item del arreglo es un operador se elimina
+  if (operador.includes(calcular[calcular.length -1])) calcular.pop();
+
+  // desde aqui se realiza el calculo y se establece para proximo calculo
+  nuevoNum = "";
+
+  calcular.forEach(e => {
+    nuevoNum = nuevoNum + e;
+  });
+
+  nuevoNum = eval(nuevoNum);
+  calcular.length = 0;
+  calcular.push(nuevoNum);
+  resultado.innerHTML = nuevoNum;
+  nuevoNum = "";
 }
 
 function handleClear() {
+  calcular.length = 0;
+  operador = "";
+  nuevoNum = ""
   resultado.innerHTML = 0;
 }
